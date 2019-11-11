58b1c101b6e79a0ed8ee1d44bbd6f6cd32f5e687
Ardakaniz <ardakaniz404@gmail.com>
2019-11-11 17:15:13 +0100

    Clock: TimePoint & Duration according to Clock type

--- Sources/Core/Clock.hpp
+++ Sources/Core/Clock.hpp
@@ -4,6 +4,6 @@
 
 namespace Core {
 	using Clock = std::chrono::steady_clock;
-	using TimePoint = std::chrono::time_point<Clock>;
-	using Duration = std::chrono::duration<float>; // In seconds, float to have more precision
+	using TimePoint = Clock::time_point;
+	using Duration = Clock::duration;
 }
