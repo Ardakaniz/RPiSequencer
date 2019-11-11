29e84cd940721c7bff4d77bcf8d9200855e3fcc9
Ardakaniz <ardakaniz404@gmail.com>
2019-11-11 17:33:40 +0100

    Recorder: fix last_release_instant

--- Sources/Core/Recorder.cpp
+++ Sources/Core/Recorder.cpp
@@ -12,17 +12,14 @@ namespace Core {
 		_step_count = step_count;
 
 		_pattern->get().second = Clock::now();
+		_last_release_point = std::nullopt;
 	}
 
 	void Recorder::Stop() {
 		if (!_pattern)
 			return;
 
-		const TimePoint last_release_instant{
-			_pattern->get().first.empty() 
-				? Clock::now()
-				: _pattern->get().first.back().release_instant
-		};
+		const TimePoint last_release_instant{ _last_release_point.value_or(Clock::now()) };
 
 		for (std::size_t i: _pressed_notes_id) // If there are still pressed notes, we make them release at the same time than the last released note or now if there arent any released note yet
 			_pattern->get().first[i].release_instant = last_release_instant;
@@ -41,16 +38,17 @@ namespace Core {
 
 		auto note = data->first;
 		auto& pattern = _pattern->get().first;
-		if (data->second) {
+		if (data->second) { // Note Pressed
 			note.pressed_instant = Clock::now();
 			pattern.push_back(note);
 			_pressed_notes_id.push_back(_pattern->get().first.size() - 1);
 		}
-		else {
+		else { // Note Released
 			auto corresponding_note_id = std::find_if(std::begin(_pressed_notes_id), std::end(_pressed_notes_id), [&note, &pattern, this](std::size_t id) { return note.note == pattern[id].note; });
 			if (corresponding_note_id != std::end(_pressed_notes_id)) { // If we find a corresponding note (same note value)
 				pattern[*corresponding_note_id].release_instant = Clock::now(); // We set its release instant
-				
+				_last_release_point = pattern[*corresponding_note_id].release_instant;
+
 				_pressed_notes_id.erase(corresponding_note_id); // And erase it from pressed notes
 			}
 		}
