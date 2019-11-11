13b3196dab09b0d7a56883324b6fff9893c3dbd5
Ardakaniz <ardakaniz404@gmail.com>
2019-11-11 12:32:10 +0100

    Note: add velocity and chan parameters

--- Sources/Core/Note.hpp
+++ Sources/Core/Note.hpp
@@ -7,8 +7,10 @@
 
 namespace Core {
 	struct Note {
-		std::uint8_t note;
+		std::uint8_t note, velocity;
 		TimePoint pressed_instant, release_instant;
+
+		std::uint8_t chan{ 0 };
 
 		inline float GetDuration() const {
 			return std::chrono::duration_cast<Duration>(release_instant - pressed_instant).count();
