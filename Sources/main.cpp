ef8accf1bb4b79201423a31eb3dc7fd7b283d772
Ardakaniz <ardakaniz404@gmail.com>
2019-11-17 16:00:23 +0100

    Update main.cpp

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,9 +1,22 @@
 #include "Core/Sequencer.hpp"
-#include "Core/Player.hpp"
-#include "Core/Recorder.hpp"
-
-#include <iostream>
+#include "Device/LogDevice.hpp"
+#include "Device/MidiInDevice.hpp"
+#include "Device/MidiOutDevice.hpp"
+#include "Controller/ImGUIController.hpp"
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
+	std::vector<std::shared_ptr<InputDevice>> input_devices{
+		std::make_shared<MidiInDevice>(),
+	};
+
+	std::vector<std::shared_ptr<OutputDevice>> output_devices{
+		std::make_shared<LogDevice>(),
+		std::make_shared<MidiOutDevice>(),
+	};
+
+	ImGUIController cont{ input_devices, output_devices };
+	Core::Sequencer seq{ cont };
+
+	while(!cont.IsStopped())
+		seq.Run();
 }
